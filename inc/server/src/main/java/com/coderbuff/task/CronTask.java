10f6dee14b03950ffb5666b51ab89c3d5296899e
yu-linfeng <hellobug@outlook.com>
2019-07-08 21:02:05 +0800

    定义定时任务

---
+++ inc/server/src/main/java/com/coderbuff/task/CronTask.java
@@ -0,0 +1,22 @@
+package com.coderbuff.task;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * 定时任务
+ * Created by OKevin on 2019-07-08 22:58
+ **/
+@Component("cronTask")
+public class CronTask {
+
+    /**
+     * 获取公司市值信息
+     *
+     */
+    @Scheduled(cron = "* * * * * ?")
+    public void fetchCompanyInfo() {
+        //TODO
+        System.out.println("定时获取");
+    }
+}
---
+++ inc/server/src/main/java/com/coderbuff/task/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * 定时服务
+ * Created by OKevin on 2019-07-08 22:55
+ **/
+package com.coderbuff.task;
